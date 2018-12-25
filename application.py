e70868cabd8baa8ea3e913065f158706f486915f
Ahmed Amir <thearabiandreamer@gmail.com>
2018-12-25 22:19:06 +0400

    added ability to add rooms and see existing universe of rooms. Need to work out memory storage of said rooms and onclick behvaiour

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request, session, jsonify, json
 
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
@@ -7,8 +7,10 @@ app.config['SECRET_KEY'] = "74561..834.51.835048.5867"
 socketio = SocketIO(app)
 
 Users = dict({})
+Users_to_rooms = dict({})
+Rooms_to_messages = dict({})
 messages = []
-private_rooms = dict({})
+Rooms = []
 
 @app.route("/")
 def entry():
@@ -41,6 +43,7 @@ def login():
         if Username != "" and Password != "":
             if not Users.get(Username):
                 Users[Username] = Password
+                Users_to_rooms[Username] = []
                 session["logged_in"] = True
                 session["User"] = Username
                 return chat()
@@ -53,13 +56,39 @@ def login():
 @app.route("/chat", methods=["POST"])
 def chat():
     User = session["User"]
-    return render_template("chat.html", Username=User, data=messages)
+    return render_template("chat.html", Username=User, data=messages, rooms=Users_to_rooms[User])
+
+@app.route("/create", methods=["POST"])
+def create():
+    User = session["User"]
+    #print("request form: ", request.data) Weird Request.data object in bytes
+    room = json.loads(request.data)["channelName"]
+    success = room not in Rooms
+    return jsonify({"success": success})
+
+@app.route("/messages", methods=["POST"])
+def __messages():
+    User = session["User"]
+    channelName = json.loads(request.data)["channelName"]
+    messages_list = Rooms_to_messages[channelName]
+    return jsonify(messages_list)
 
 @socketio.on("message_sent")
 def handle_message(data):
     print(data)
     messages.append(data)
-    emit("message_received", data, broadcast="True", room=private_rooms[data.Username])
+    emit("message_received", data, broadcast="True")
+
+@socketio.on("channel_created")
+def handle_creation(data):
+    print(data)
+    room = data["channelName"]
+    User = session["User"]
+    Rooms.append(room)
+    Users_to_rooms[User].append(room)
+    Rooms_to_messages[room] = []
+    print(Users_to_rooms)
+    emit("channel_was_created", {"channelName": data["channelName"], "User": User}, broadcast=True)
 
 @socketio.on("join")
 def join(data):
@@ -67,7 +96,7 @@ def join(data):
     username = data.Username
     print(room, username)
     join_room(room)
-    private_rooms[username] = room
+    Rooms[username] = room
 
 @app.route("/logout")
 def logout():
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,7 +1,98 @@
 $(() => {
     const message_template = Handlebars.compile(document.getElementById("message-template").innerHTML)
-
     const socket = io();
+
+    // sidebar menu html behaviour
+
+    const menu_trigger = document.querySelector(".menu__trigger")
+    let is_menu_open = true
+    const menu = document.querySelector(".menu--wrapper")
+    menu_trigger.onclick = () => {
+        if (is_menu_open) {
+            menu.style.marginLeft = "-300px"
+            is_menu_open = false;
+            menu_trigger.style.transform = "rotate(180deg)";
+        } else {
+            menu.style.marginLeft = "0px"
+            is_menu_open = true;
+            menu_trigger.style.transform = "rotate(0deg)";
+        }
+    }
+    
+    const button_myrooms = document.querySelector("#button_myrooms")
+    const button_join = document.querySelector("#button_join")
+    const button_create = document.querySelector("#button_create")
+    const menu_grid = document.querySelector(".menu__grid")
+
+    button_myrooms.onclick = () => { menu_grid.style.left = "0px" }
+    button_join.onclick = () => { menu_grid.style.left = "-300px" }
+    button_create.onclick = () => { menu_grid.style.left = "-600px" }
+
+    // sidebar back-end
+
+    const createChannelForm = document.querySelector("#createChannelForm");
+    createChannelForm.onsubmit = (e) => {
+        e.preventDefault();
+        const channelNameInput = document.getElementsByName("channelName")[0]
+        const channelName = channelNameInput.value;
+        const emptyError = document.querySelector("#createChannelForm--empty")
+        const uniqueError = document.querySelector("#createChannelForm--unique")
+        //console.log(channelName)
+        if (channelName == "") { emptyError.classList.remove("d-none");uniqueError.classList.add("d-none"); return false} 
+        else { emptyError.classList.add("d-none");uniqueError.classList.add("d-none") }
+        
+        fetch(url="/create", {method: 'POST',
+                        //headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify({"channelName": channelName})
+                    })
+        .then(function json(response) {return response.json()} ).then(
+            function(data) {
+                
+                //console.log(data["success"])
+                if (data["success"] == true) {
+                    socket.emit("channel_created", {"channelName": document.getElementsByName("channelName")[0].value, 
+                    "User": document.getElementById("chatbox").children[0].value });
+                    // logic for adding the channel into myrooms
+
+                    const myRoomsMenu = document.querySelector(".menu__grid__myrooms__rooms")
+                    const newChannel = document.createElement("div")
+                    newChannel.setAttribute("class", "menu__grid__myrooms__rooms__item")
+
+                    newChannel.onclick = () => {
+                        fetch(url="/messages", {method: 'POST',
+                        body: JSON.stringify({"channelName": document.getElementsByName("channelName")[0].value}))
+                        .then(function json_2(response) {return resposne.json()})
+                        .then(function(data_2) {
+                            const message = data_2["messages"]
+                            
+                        })
+                    }
+                    newChannel.innerHTML = document.getElementsByName("channelName")[0].value;
+                    myRoomsMenu.append(newChannel)
+
+                    document.getElementsByName("channelName")[0].value = ""
+                    // TODO: add onclick behaviour.
+                } else {uniqueError.classList.remove("d-none")}
+            }
+        )
+    }
+
+    socket.on("channel_was_created", data => {
+        const User = data.User
+        const channelName = data.channelName
+        console.log(User, channelName)
+
+        if (User != document.getElementById("chatbox").children[0].value) {
+            const joinMenu = document.querySelector(".menu__grid__join__rooms")
+            const newChannel = document.createElement("div")
+            newChannel.setAttribute("class", "menu__grid__join__rooms__item")
+            newChannel.innerHTML = channelName;
+            joinMenu.append(newChannel)
+
+            // TODO: add onclick behaviour
+        }
+    })
+
     const message_form = document.getElementById("chatbox")
     message_form.onsubmit = (e) => {
         e.preventDefault();
@@ -24,16 +115,4 @@ $(() => {
         message_list.appendChild(message_row)
     })
 
-    const joinRoom_button = document.querySelector(".join")
-    const room_list = document.querySelector("#RoomList")
-    joinRoom_button.onclick = () => {
-        if (room_list.classList.contains("d-hidden")) { room_list.classList.remove("d-hidden") } else { room_list.classList.add("d-hidden") }
-    }
-
-    rooms = room_list.children
-    for (var i=0; i<rooms.length; i++) {
-        rooms[i].onclick = () => {
-            socket.emit("join", {"Username": message_form.children[0].value, "Room": rooms[i].text})
-        }
-    }
 })
