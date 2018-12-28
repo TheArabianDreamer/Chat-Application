aecbd23ce6f8074ba63a718f09e644b58701c21b
Ahmed Amir <thearabiandreamer@gmail.com>
2018-12-28 16:01:41 +0400

    chatApp complete

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ app.config['SECRET_KEY'] = "74561..834.51.835048.5867"
 socketio = SocketIO(app)
 
 Users = dict({})
+Users_to_active = dict({})
 Users_to_rooms = dict({})
 Rooms_to_messages = dict({})
 messages = []
@@ -44,6 +45,7 @@ def login():
             if not Users.get(Username):
                 Users[Username] = Password
                 Users_to_rooms[Username] = []
+                Users_to_active[Username] = None
                 session["logged_in"] = True
                 session["User"] = Username
                 return chat()
@@ -56,7 +58,16 @@ def login():
 @app.route("/chat", methods=["POST"])
 def chat():
     User = session["User"]
-    return render_template("chat.html", Username=User, data=messages, rooms=Users_to_rooms[User])
+    print("Is User in Room? ", Users_to_active[User] != None)
+    print("which room? ", Users_to_active[User])
+    if Users_to_active[User] != None:
+        myRoom = Users_to_active[User]
+        myRoomMessages = Rooms_to_messages[myRoom]
+        print(myRoom)
+    else:
+        myRoom = None
+        myRoomMessages = []
+    return render_template("chat.html", Username=User, myRoom=myRoom, messages=myRoomMessages, rooms=Users_to_rooms[User], join_rooms=[room for room in Rooms if room not in Users_to_rooms[User]])
 
 @app.route("/create", methods=["POST"])
 def create():
@@ -69,15 +80,40 @@ def create():
 @app.route("/messages", methods=["POST"])
 def __messages():
     User = session["User"]
+    print(json.loads(request.data))
     channelName = json.loads(request.data)["channelName"]
+    #join_room(channelName)  Instead, make a socket emit after the /messages call. Can't do socket functions in a view function.
+    print(Rooms_to_messages)
     messages_list = Rooms_to_messages[channelName]
+    print(jsonify(messages_list))
     return jsonify(messages_list)
+
+@app.route("/join", methods=["POST"])
+def include():
+    data = json.loads(request.data)
+    room = data["channelName"]
+    User = session["User"]
+    Users_to_rooms[User].append(room)
+    return jsonify({"success": True})
+
+@socketio.on("channel_opened")
+def channel_opened(data):
+    print("rougue channel_opened data: ", data)
+    room = data["channelName"]
+    username = session["User"]
+    if Users_to_active[username] != None:
+        leave_room(Users_to_active[username])
+    print(room, username)
+    join_room(room)
+    Users_to_active[username] = room
+    
 
 @socketio.on("message_sent")
 def handle_message(data):
     print(data)
-    messages.append(data)
-    emit("message_received", data, broadcast="True")
+    room = Users_to_active[session["User"]]
+    Rooms_to_messages[room].append(data)
+    emit("message_received", data, broadcast="True", room=Users_to_active[session["User"]])
 
 @socketio.on("channel_created")
 def handle_creation(data):
@@ -90,13 +126,6 @@ def handle_creation(data):
     print(Users_to_rooms)
     emit("channel_was_created", {"channelName": data["channelName"], "User": User}, broadcast=True)
 
-@socketio.on("join")
-def join(data):
-    room = data.Room
-    username = data.Username
-    print(room, username)
-    join_room(room)
-    Rooms[username] = room
 
 @app.route("/logout")
 def logout():
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,29 @@
 $(() => {
+
+    /*
+
+    TODO:
+
+    1 - Need to add the chat in session into flask app.
+    2 - Need to getMessages for said chat.
+    DONE!! NOT!!
+
+    New issues:
+    1' - Need to open socket connection on opening an outsider chat
+    DONE!!
+
+    3 - Need to fix the layout of the messages__history. Maybe a little ugly?
+     -> Maybe add a bubble!
+     DONE!!
+
+
+
+     
+    4 - Need to fix multiple chats
+    -> More info: Happens for people who create chats, talk in them, and then log out!
+
+    */
+
     const message_template = Handlebars.compile(document.getElementById("message-template").innerHTML)
     const socket = io();
 
@@ -53,28 +78,74 @@ $(() => {
                     socket.emit("channel_created", {"channelName": document.getElementsByName("channelName")[0].value, 
                     "User": document.getElementById("chatbox").children[0].value });
                     // logic for adding the channel into myrooms
-
+                    const channelName = document.getElementsByName("channelName")[0].value;
                     const myRoomsMenu = document.querySelector(".menu__grid__myrooms__rooms")
                     const newChannel = document.createElement("div")
-                    newChannel.setAttribute("class", "menu__grid__myrooms__rooms__item")
 
-                    newChannel.onclick = () => {
-                        fetch(url="/messages", {method: 'POST',
-                        body: JSON.stringify({"channelName": document.getElementsByName("channelName")[0].value}))
-                        .then(function json_2(response) {return resposne.json()})
-                        .then(function(data_2) {
-                            const message = data_2["messages"]
-                            
-                        })
-                    }
+
+                    newChannel.setAttribute("class", "menu__grid__myrooms__rooms__item")
+                    newChannel.setAttribute("data", `name: ${channelName}`)
                     newChannel.innerHTML = document.getElementsByName("channelName")[0].value;
                     myRoomsMenu.append(newChannel)
 
+                    //renew important consts
+                    const myRoomsMenuAddition = myRoomsMenu.children[myRoomsMenu.children.length - 1]
+                    console.log(myRoomsMenuAddition)
+                    myRoomsMenuAddition.addEventListener('click', getMessages, false)
+                    myRoomsMenuAddition.node = myRoomsMenuAddition.innerHTML
+                    /*
+
+                    KEEP IN MIND:
+
+                    the onclick function is actually being called. the onclick needs to seperately be assigned to getMessages()
+                    DONE!
+                    */
+                    
                     document.getElementsByName("channelName")[0].value = ""
-                    // TODO: add onclick behaviour.
+                    // TODO: add onclick behaviour. DONE!
+                    // TODO: add onclick behaviour to chat on startup.
                 } else {uniqueError.classList.remove("d-none")}
             }
         )
+    }
+
+    function getMessages(event) {
+        console.log("Someone Entered here")
+        const channelName = this.innerHTML
+        console.log(this.innerHTML)
+        console.log(channelName)
+        fetch(url="/messages", {method: 'POST',
+            body: JSON.stringify({"channelName": channelName})})
+        .then(function json(response) { return response.json() })
+        .then(function(data) {
+            const messages = data
+            console.log(data)
+            const messages_container = document.querySelector("#message")
+            messages_container.querySelector("h1").innerHTML = channelName
+            const messages_history = document.querySelector(".messages__history")
+            messages_history.innerHTML = ""
+            for (var i=0; i < messages.length; i++) {
+                const chatHistoryTemplate = message_template(messages[i])
+                const messageRow = document.createElement("div")
+                messageRow.setAttribute("class", "message__item")
+                messageRow.innerHTML = chatHistoryTemplate
+                messages_history.append(messageRow)
+            }
+            // important!! 
+            // need to add a div around the messages so that getMessages for a new channel can OVERWRITE existing messages. done!!
+            socket.emit("channel_opened", {"channelName": channelName})
+            //also emit channel_opened.
+            // need to emit join, so that join_room can be called in order to have rooms in chats. done!!
+
+            // LATEST IMPORTANT:
+            // url /messages returns only a list of messages. need a list of objects. DONE!!
+
+        })
+    }
+
+    const allMyRooms = document.querySelectorAll(".menu__grid__myrooms__rooms__item")
+    for (var i = 0; i < allMyRooms.length; i++) {
+        allMyRooms[i].onclick = getMessages
     }
 
     socket.on("channel_was_created", data => {
@@ -88,10 +159,41 @@ $(() => {
             newChannel.setAttribute("class", "menu__grid__join__rooms__item")
             newChannel.innerHTML = channelName;
             joinMenu.append(newChannel)
-
+            const lastChild = joinMenu.children[joinMenu.children.length - 1]
+            lastChild.onclick = joinNewMenu
             // TODO: add onclick behaviour
         }
     })
+
+    function joinNewMenu(event) {
+        const channelName = this.innerHTML;
+        const channel = this
+        console.log(this)
+        fetch(url="/join", {method: "POST",
+            body: JSON.stringify({"channelName": channelName})})
+        .then(function json(response) { return response.json() })
+        .then(function(data) {
+            const myRoomsMenu = document.querySelector(".menu__grid__myrooms__rooms")
+            const Addition = document.createElement("div")
+            const joinMenu = document.querySelector(".menu__grid__join__rooms")
+            channel.parentNode.removeChild(channel)
+            Addition.setAttribute("class", "menu__grid__myrooms__rooms__item")
+            Addition.innerHTML = channelName
+            Addition.onclick = getMessages
+            myRoomsMenu.append(Addition)
+
+        })
+    }
+
+    const allJoinMenus = document.querySelectorAll(".menu__grid__join__rooms__item")
+    for (var i=0; i < allJoinMenus.length; i++) {
+        allJoinMenus[i].onclick = joinNewMenu
+    }
+
+    const isUserInLegacySession = document.getElementById("isUserInLegacySession")
+    if (isUserInLegacySession.value == "True") {
+        socket.emit("channel_opened", {"channelName": isUserInLegacySession.dataset.room})
+    }
 
     const message_form = document.getElementById("chatbox")
     message_form.onsubmit = (e) => {
@@ -108,10 +210,10 @@ $(() => {
         const context = data;
         const message_html = message_template({"Username": data["Username"], "msg": data["msg"]})
         const message_row = document.createElement("div")
-        message_row.setAttribute("class", "row")
+        message_row.setAttribute("class", "message__item")
         console.log(message_html)
         message_row.innerHTML = message_html;
-        const message_list = document.getElementById("message")
+        const message_list = document.querySelector(".messages__history")
         message_list.appendChild(message_row)
     })
 
